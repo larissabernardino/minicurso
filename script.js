3a3d92f28b26e78bad7049b79821ed7ec363e248
Larissa <bernardinolarissaa@gmail.com>
2022-12-04 19:56:11 -0300

    Corrigir html,adicionar estilo para fotos,criar consumo de api

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-const imagem = document.querySelection('img');
-const botao = document.querySelection('button');
-const nomeDoPersonagem = document.querySelection('#nome');
-const especie = document.querySelection('#especie');
-const status = document.querySelection('#status');
+const imagem = document.querySelector('img');
+const botao = document.querySelector('button');
+const nomeDoPersonagem = document.querySelector('#nome');
+const especie = document.querySelector('#especie');
+const condicao = document.querySelector('#status');
+
+gerarValorAleatorio = () => {
+    return Math.floor(Math.random() * 671);
+}
+
+pegarPersonagem = () => {
+    let numeroAleatorio = gerarValorAleatorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method:'GET',
+        headers:{
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt = data.name;
+        nomeDoPersonagem.innerHTML = data.name;
+        especie.innerHTML = data.especie;
+        condicao.innerHTML = data.status;
+
+    });
+}
+
+botao.onclick = pegarPersonagem;
+
