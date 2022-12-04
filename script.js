4ccb24cd0f748dc5d68bae594a7c49ffe85e250e
Larissa <bernardinolarissaa@gmail.com>
2022-12-04 19:21:49 -0300

    Capturar elementos html manipulaveis

---
+++ script.js
@@ -0,0 +1,5 @@
+const imagem = document.querySelection('img');
+const botao = document.querySelection('button');
+const nomeDoPersonagem = document.querySelection('#nome');
+const especie = document.querySelection('#especie');
+const status = document.querySelection('#status');
